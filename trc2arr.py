972e305fe68229ddd1964ff5d964694391645812
babetCode <adrienbabet1@gmail.com>
2024-07-16 20:30:42 -0700

    trc2arr
    version 2 from desktop

--- trc2arr.py
+++ trc2arr.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+import math
 
-#function which takes openCap .tc marker data and outputs tuple with (header dictionary, pandas arr)
+
+##  function which takes openCap .trc marker data and outputs marker-->xyz no. dictionary, pandas arr
+
 def trc_pd(trcfile):
     with open(trcfile) as fd:
         #read file into list
@@ -13,13 +16,20 @@ def trc_pd(trcfile):
         pos = lines[4].rstrip().split('\t')[2:]
         xyz = [pos[i:i+3] for i in range(0, len(pos), 3)]
         dct = dict(zip(markers, xyz))
-        #make array
-        header = 'Frame'+'\t'+'Time'+'\t'+lines[4].lstrip()
-        lines = [header]+lines[6:]  
+        #make header for df
+        header = 'Frame#'+'\t'+'Time'+'\t'+lines[4].lstrip()
+        header = header.rstrip().split('\t')
+        #make body of df
+        lines = lines[6:]   
         lines2d = [l.rstrip().split('\t') for l in lines]
-        print(lines2d[0])
         df = pd.DataFrame(lines2d)
+        #add header
+        df.columns = header
+        #returen tuple of dct, df
         return dct, df
+
+
+##  function which takes openCap .trc marker data and outputs xyz no.-->array index dictionary, numpy arr
 
 def trc_np(trcfile):
     with open(trcfile) as fd:
@@ -35,17 +45,77 @@ def trc_np(trcfile):
         arr = np.array(linesfl)
         return dct, arr
 
+    
+##  find midpoint of two points, returns [x,y,z] list
+    
+def midpt(a,b):
+    return [(float(a[0])+float(b[0]))/2, (float(a[0])+float(b[0]))/2, (float(a[0])+float(b[0]))/2]
+
+
+##  find the vector from a to b, returns [x,y,z] list
+
+def vec(a,b):
+    return[(float(b[0]))-float(a[0]), (float(b[1])-float(a[1])), (float(b[2])-float(a[2]))]
+
+
+##  calculate pitch, yaw, roll from xyz pozitions of three points
+
+def pyr(a,b,c):
+    abm = midpt(a,b)
+    v = vec(c, abm)
+    y = math.atan(v[0]/v[2])
+    x = math.atan(v[2]/v[1])
+    z = math.atan(v[1]/v[0])
+    return [x,y,z]
+    
+
+##  calculate distance between two points
+
+def point_diff(a, b):
+    dis = 0.0
+    for i in range(3):
+        dis += (float(b[i])-float(a[i]))**2
+    distance = math.sqrt(dis)
+    return distance
 
 def main():
+    
     #get header dictionary as dct and pandas data frame as df
-    dct, arr = trc_np('libraryWalk2.trc')
+    dct, df = trc_pd('PyLibWalk2.trc')
 
-    for ct, i in enumerate(arr[:,:2]):
-        if ct >=10:
-            break
-        print(ct, i*2)
+##    print(df.iloc[0,0])
+##    print(df)
+
+##    for x, y in df[['X4', 'Y4', 'Z4']]:
+##        print(x, y)
+
+##    for c, i in enumerate(df['Time']):
+##        print(i)
+##        if c > 10:
+##            break
+##
+##    print(df[['Frame#', 'Time']])
+##
+    for a,b,c,d,e,f,g,h,i in zip(df['X62'], df['Y62'], df['Z62'], df['X63'], df['Y63'], df['Z63'], df['X60'], df['Y60'], df['Z60']):
+        lat = [a,b,c]
+        med = [d,e,f]
+        elb = [g,h,i]
+        print(pyr(lat,med,elb))
+    print(pyr([0,0,0], [0,0,0], [9,9,9]))
+
+##    for a,b,c in zip(df['X62'], df['Y62'], df['Z62']):
+##        print(a)
+##        #print('{}, {}'.format(i, j))
+
+    #print(df[['X4', 'Y4', 'Z4']])
 
 
-   
+##    print(dct.keys())
+##    print(df.loc[df['Frame#'] == '3'])
+##    print(df.sort_values('Z4'))
+    #for index, row in df.iterrows():
+    #    print(row)
+
+    
 if __name__ == "__main__":
     main()
